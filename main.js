08409b9290728ce97f7f05428a1f9707192416ba
AttaTechyKid <128951217+AttaTechyKid@users.noreply.github.com>
2023-08-27 17:29:43 -0700

    ljkrwanlkjdsbkdsjnlkjdsgjdsnljxbljdsnvhjbvfdnkgijbjbdrnjhnsjsd

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 // Extract the query parameter from the URL
 function getQueryParam(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
-  }
-  
-  // Open the provided link in a new tab
-  function openLinkInNewTab(link) {
-    window.open(
-      link, "_blank");
-  }
-  
-  // Redirect back to the original page after a delay
-  function redirectBack(current) {
-    setTimeout(() => {
-      window.location.href = current;
-    }, 1000);
-  }
-  
-  // Get the link and current URL from query parameters
-  const linkParam = getQueryParam('link');
-  const currentParam = getQueryParam('current');
-  
-  // Check if the link parameter is provided
-  if (linkParam) {
-    openLinkInNewTab(linkParam);
-  
-    // Check if the current parameter is provided
-    if (currentParam) {
-      redirectBack(currentParam);
-    }
-  }
-  
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+// Open the provided link in a new tab
+function openLinkInNewTab(link) {
+  window.open(link, '_blank');
+}
+
+// Redirect back to the original page after a delay
+function redirectBack(current) {
+  setTimeout(() => {
+    window.location.href = current;
+  }, 1000);
+}
+
+// Get the link and current URL from query parameters
+const linkParam = getQueryParam('link');
+const currentParam = getQueryParam('current');
+
+// Check if the link parameter is provided
+if (linkParam) {
+  openLinkInNewTab(linkParam);
+}
+
+// Check if the current parameter is provided
+if (currentParam) {
+  redirectBack(currentParam);
+}
